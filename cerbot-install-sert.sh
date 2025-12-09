afd850157fdb99b1b5ad65ccb076f186e16b2338
jlebiannic <johann.lebiannic@soprasteria.com>
2025-12-09 14:42:21 +0100

    feat: Add Nginx configuration and Certbot script for SSL setup in Docker environment

---
+++ cerbot-install-sert.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+# Script to install certbot and obtain SSL certificates for a given domain
+sudo certbot certonly \
+  --webroot -w ./certbot/www
+  -d wishgifthub.com \
+  --email jlebiannic@gmail.com
+  --agree-tos
+  --no-eff-email
